31488834d11abb9f8b15e138f6241f53881d55e0
ashp8 <ashishira521@gmail.com>
2024-09-06 11:33:53 +0600

    Init

---
+++ Makefile
@@ -0,0 +1,38 @@
+# Compiler and flags
+CFLAGS = -std=c++17 -O2
+LDFLAGS = -lglfw -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi -lvulkan
+
+# Directories
+SRC_DIR = src
+INC_DIR = include
+BUILD_DIR = build
+
+# Vulkan SDK paths
+INC = -I${VULKAN_SDK}/include -I$(INC_DIR)
+LIB = -L${VULKAN_SDK}/lib/
+# Source and object files
+SRCS = $(wildcard $(SRC_DIR)/*.cpp)
+OBJS = $(patsubst $(SRC_DIR)/%.cpp, $(BUILD_DIR)/%.o, $(SRCS))
+
+# Target executable
+TARGET = VulkanTest
+
+# Build the target
+$(TARGET): $(OBJS)
+	g++ $(CFLAGS) -o $@ $(OBJS) $(LIB) $(LDFLAGS)
+
+# Rule to compile source files into object files
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp
+	mkdir -p $(BUILD_DIR)
+	g++ $(CFLAGS) $(INC) -c $< -o $@
+
+# Phony targets
+.PHONY: test clean
+
+# Test target
+test: $(TARGET)
+	./$(TARGET)
+
+# Clean target
+clean:
+	rm -f $(BUILD_DIR)/*.o $(TARGET)
---
+++ include/application.h
@@ -0,0 +1,92 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <cstring>
+#include <optional>
+#include <map>
+#include <set>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete(){
+        return graphicsFamily.has_value() && presentFamily.has_value();
+    }
+};
+
+class Application {
+public:
+    void run();
+
+private:
+    void initMainwindow();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+    void createInstance();
+    bool checkValidationLayerSupport();
+    std::vector<const char*> getRequiredExtensions();
+    void setupDebugMessenger();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createSurface();
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    bool isDeviceSuitable(VkPhysicalDevice device);
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+    void createSwapChain();
+    void createImageViews();
+    void createGraphicsPipeline();
+
+    
+private:
+    VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicsQueue;
+    VkSurfaceKHR surface;
+    VkQueue presentQueue;
+    VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
+
+
+    GLFWwindow* window;
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    const std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    VkDebugUtilsMessengerEXT debugMessenger;
+
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+};
